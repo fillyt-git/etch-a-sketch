d779989309602b2b1f9a367255018264d8dc2605
fillyt-git <156151778+fillyt-git@users.noreply.github.com>
2024-02-07 12:33:20 -0600

    Add grid count prompt

--- script.js
+++ script.js
@@ -1,26 +1,55 @@
-/* const gridCount = 16; */
+let count = 16
 const mainContainer = document.querySelector('#mainContainer');
-const gridDivs = [];
-const gridRowDivs = [];
+let gridDivs = [];
+let gridRowDivs = [];
+let allRows;
+let allSquares;
 
-for (let i = 0; i < 16; i++) {
-    gridRowDivs.push(document.createElement('div'));
-    gridRowDivs[i].classList.add('row');
-    gridRowDivs[i].setAttribute('id', `row-${i}`);
-    mainContainer.appendChild(gridRowDivs[i]);
 
-    gridDivs[i] = [];
-        for (j = 0; j < 16; j++) {
-            gridDivs[i].push(document.createElement('div'));
-            gridDivs[i][j].classList.add('square');
-            gridDivs[i][j].setAttribute('id', `square-${i}-${j}`);
-            gridRowDivs[i].appendChild(gridDivs[i][j]);
-        };
+function setGrid (count) {
+    for (let i = 0; i < count; i++) {
+        gridRowDivs.push(document.createElement('div'));
+        gridRowDivs[i].classList.add('row');
+        gridRowDivs[i].setAttribute('id', `row-${i}`);
+        mainContainer.appendChild(gridRowDivs[i]);
+
+        gridDivs[i] = [];
+            for (j = 0; j < count; j++) {
+                gridDivs[i].push(document.createElement('div'));
+                gridDivs[i][j].classList.add('square');
+                gridDivs[i][j].setAttribute('id', `square-${i}-${j}`);
+                gridRowDivs[i].appendChild(gridDivs[i][j]);
+            };
+    };
+
+    allRows = document.querySelectorAll('.row');
+    allSquares = document.querySelectorAll('.square');
+
+    allSquares.forEach((square) => {
+        square.addEventListener('mouseover', (e) => {
+            e.target.style.cssText = 'background-color: white;';
+        });
+    });
 };
 
-const allSquares = document.querySelectorAll('.square');
-allSquares.forEach((square) => {
-    square.addEventListener('mouseover', (e) => {
-        e.target.style.cssText = 'background-color: white;';
+setGrid(count);
+
+function clearGrid () {
+    allSquares.forEach((square) => {
+        square.remove();
     });
+    allRows.forEach((row) => {
+        row.remove();
+    });
+};
+
+const gridCount = document.querySelector('#gridCount');
+gridCount.addEventListener('click', () => {
+    count = prompt('How many squares per side? (Limit: 100)', 16) || count;
+    while (count > 100 || count < 1) {
+        count = prompt('How many squares per side? (Limit: 100)', 16) || count;
+    };
+    clearGrid();
+    console.log(gridDivs);
+    setGrid(count);
 });
